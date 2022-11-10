aec97659c793a20a6a4a222d510a705cff985579
sergiopele <sergiypele@gmail.com>
2022-11-09 22:33:57 -0500

    "ticket selection page (AIR CANADA"

--- src/main/java/examples/AuthenticationPopup.java
+++ src/main/java/examples/AuthenticationPopup.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AuthenticationPopup {
+	public static String adminLog = "admin";        //-reading from XLS or CSV
+	public static String passLog = "admin";            //reading from some external XLS or CSV
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/Users/sergiopele/Documents/extra_library_for_intellij/chromedriver");
 		WebDriver driver = new ChromeDriver();
@@ -11,7 +14,7 @@ public class AuthenticationPopup {
 		//credential in this particular example - admin, pass - admin
 		//original url:https://the-internet.herokuapp.com/basic_auth
 		// we simply add https://admin:admin@the-internet.herokuapp.com/basic_auth
-		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+		driver.get("https://"+adminLog+":"+passLog+"@the-internet.herokuapp.com/basic_auth");
 		
 		
 	}
